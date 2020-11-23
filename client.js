7ce3f273823f5730f14e5b63a0905db6cc4b0dc4
luaugusto@laposte.net <luaugusto@laposte.net>
2020-11-23 11:13:04 +0100

    connected getHotels and cancelResservation to rest API

--- client.js
+++ client.js
@@ -1,11 +1,23 @@
 var soap = require('soap');
-var url = 'http://localhost:8000/wsdl?wsdl';
-var args = {rentalDate: "05-09-2020",numberOfNights:1,numberOfRoom:1};
+var url = 'http://localhost:8081/wsdl?wsdl';
+var args = {idReservation: "toto"};
+const axios = require('axios');
 
+var params = {
+
+  identifier : "toto",
+  name : "toto",
+  surname : "toto",
+  start_date : "24_11_2020",
+  nights : 5,
+  hotel_identifier : "titi"
+
+}
+
+//axios.post("http://localhost:8082/bookings",params);
 
 soap.createClient(url,function(err,client) {
-    client.getHotels(args,function(err,result) {
+    client.cancelReservation(args,function(err,result) {
         console.log(result);
-        console.log(client.describe());
       });
 });
--- index.js
+++ index.js
@@ -1,26 +1,64 @@
 const express = require('express');
 const soap = require('soap');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const config = require("./config.json");
+const axios = require('axios');
+
+var url = "http://"+config.host+":"+config.port;
+
+console.log(url);
 
 var hotelReservationService = {
     service: {
       port: {
-        getHotels: function(args) {
-          console.log("toto");  
-          return {
-            hotels: {}
-          };
+        getHotels: function(args,callback) {
+
+          var params = {
+            start_date: args.rentalDate,
+            nights: args.numberOfNights,
+            rooms: args.numberOfRooms
+          }
+
+          axios.get(url+"/ask_booking",{params:params}).then(resp=> {
+
+            if(resp.status==200){
+              callback(resp.data.data);
+            }else{
+              callback({
+                code: resp.status,
+                message: resp.data.message
+              })
+            }
+
+          })
+          .catch( (error) => {
+
+            console.log(error);
+            callback({
+              status:error.response.status,
+              message: error.response.data.message
+            })
+
+          })
+
         },
         reserverHotel: function(args) {
             return {
                 reservation: "Yes "+args.idHotel
             };
         },
-        cancelReservation : function(args) {
+        cancelReservation : function(args,callback) {
 
-            return{
-                cancellation: "Yes "+args.idHotel
-            };
+          axios.delete(url+"/booking/"+args.idReservation).then(resp=>{
+
+            callback({cancellation:"true"});
+
+          }).catch(error => {
+
+            console.log(error);
+            callback({cancellation:"false"});
+
+          })
 
         }
       }
@@ -34,7 +72,7 @@ var xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
 var app = express();
 //body parser middleware are supported (optional)
 app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
-app.listen(8000, function(){
+app.listen(8081, function(){
     //Note: /wsdl route will be handled by soap module
     //and all other routes & middleware will continue to work
     soap.listen(app, '/wsdl', hotelReservationService, xml, function(){
